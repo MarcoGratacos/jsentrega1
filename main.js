f5ca7e7bef831aae794472508c376f0b4c773fed
MarcoGratacos <msgratacos@gmail.com>
2021-10-20 21:36:13 -0300

    primer entrega

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+let nombreIngresado = prompt ("ingresa tu nombre")
+let apellidoIngresado = prompt ("ingresa tu apellido")
+let nacimientoIngresado = prompt ("ingresa tu fecha de nacimiento")
+
+alert(nombreIngresado + " " + apellidoIngresado +" " + nacimientoIngresado)
+
+
+alert("hola mundo")
+console.log("mensaje de prueba")
+
+
